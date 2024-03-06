70dee81f7087055d11e48961f73a11d67318b8c8
ZZRzan <103818163+ZZRzan@users.noreply.github.com>
2024-03-06 10:26:22 +0200

    Add files via upload

---
+++ access/values.js
@@ -0,0 +1,24 @@
+// Активировать документы (true - включены, false - отключены)
+var isRightsEnabled = true; // Права
+var isCovidCertificateEnabled = false; // COVID-сертификат
+
+// Общее
+var birthdate = "13.02.2006"; // Дата рождения
+var fullname = "Занін Максим Олександрович" // ФИО
+var name = "Максим" // Имя
+
+// Паспорт
+var passport_id = "123456789" // Номер паспорта
+
+// КПП
+var kpp_id = "0123456789" // ИНН
+
+// Права
+var rights_valid_until = "01.04.2028" // Права "Дійсні до"
+var rights_categories = "A1, A, B" // Права "Категорії"
+var rights_tsc = "ТСЦ 8631" // Права "Видав
+var rights_id = "KBE968639" // Права "Серія та номер""
+
+// COVID-сертификат
+var covid_valid_until = "01.01.2022" // Сертификат "Дійсний до"
+var covid_certificate_id = "URN:UVCI:01:UA:0<br>E55669376876888<br>B9E2520C4F88930" // Номер сертификата (<br> - перенос строки)
